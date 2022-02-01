c84d24afa9ebc4878df9ec4dfe3a1350694c58a3
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-02-01 12:35:06 +0330

    Shorhand of defining variable

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Getting started with Go
 
 [Go by Example](https://gobyexample.com/)
+
+## Run
+
+```console
+go run src/main.go
+```
--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var s string = "Hello, World!"
+	// var s string = "Hello, World!"
+	// var s = "Hello, World!"
+	s := "Hello, World!"
 	fmt.Println(s)
 }
