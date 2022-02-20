4f41beae0915bd22616242f32f527192f11aeef0
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-02-20 13:04:06 +0330

    For loops

--- src/main.go
+++ src/main.go
@@ -11,12 +11,19 @@ package main
  */
 import "fmt"
 
+// Global variables
+var x int = 20
+
+// Global constants
+const pi float64 = 3.14159265358979
+const hello = "Hello, World!"
+
 func main() {
 
 	// Different methods for defining variables:
-	var s1 string = "Hello, World! 1"
-	var s2 = "Hello, World! 2"
-	s3 := "Hello, World! 3"
+	var s1 string = hello + " 1"
+	var s2 = hello + "  2"
+	s3 := hello + " 3"
 
 	fmt.Println(s1)
 	fmt.Println(s2)
@@ -30,10 +37,12 @@ func main() {
 	var i1 int = 10
 	var i2 = 20
 	i3 := 3.14
+	i4, i5 := 40, 6
 
 	fmt.Println(i1)
 	fmt.Println(i2)
 	fmt.Println(i3 * float64(i2))
+	fmt.Println(i4 / i5)
 
 	// Define some boolean variables:
 	var b1 bool = true
@@ -44,4 +53,34 @@ func main() {
 	fmt.Println(!b2)
 	fmt.Println(b3)
 
+	// Define a local constant:
+	const g = 9.8
+
+	fmt.Println("\nFor Loop:")
+	// For loops:
+	for i := 0; i < 30; i++ {
+
+		if i == 6 {
+			// Override 6th iteration:
+			continue
+		}
+
+		if i%2 == 0 {
+			fmt.Println(i, "is Even")
+		} else {
+			fmt.Println(i, "is Odd")
+		}
+
+		if i == 10 {
+			// Break out of the loop at 10th iteration:
+			break
+		}
+	}
+
+	// Another way to define a for loop:
+	j := 12
+	for j < 40 {
+		fmt.Println(j)
+		j += 6
+	}
 }
