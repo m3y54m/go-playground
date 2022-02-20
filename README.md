2672bd581e44a055873be41c7e03e7bc8844f385
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-02-20 11:29:20 +0330

    Print strings

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Getting started with Go
 
 [Go by Example](https://gobyexample.com/)
+[An Introduction to Programming in Go](https://www.golang-book.com/books/intro)
+
+## Install Go Compiler (in Ubuntu)
+
+```console
+sudo apt install golang
+```
 
 ## Run
 
+Build and run the program in one shot:
+
 ```console
 go run src/main.go
+```
+
+## Build
+
+```console
+go build src/main.go
 ```
--- src/main.go
+++ src/main.go
@@ -1,10 +1,29 @@
+// The first statement in a Go source file must be
+// package name.
+// Executable commands must always use package main.
 package main
 
+/*
+ * The import keyword is how we include code from
+ * other packages to use with our program.
+ * The fmt package (shorthand for format) implements
+ * formatting for input and output.
+ */
 import "fmt"
 
 func main() {
-	// var s string = "Hello, World!"
-	// var s = "Hello, World!"
-	s := "Hello, World!"
-	fmt.Println(s)
+
+	// Different methods for defining variables:
+	var s1 string = "Hello, World! 1"
+	var s2 = "Hello, World! 2"
+	s3 := "Hello, World! 3"
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+
+	s1 = "New " + s1
+
+	fmt.Println(s1)
+
 }
